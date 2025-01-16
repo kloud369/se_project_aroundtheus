6a8d25f4dd6e564ec1bb5b31b69ccb20aad03936
Klaudia Kawka <klaudiajkawka@gmail.com>
2025-01-15 20:19:35 -0800

    Sprint 4 Finished Final Project

--- README.md
+++ README.md
@@ -1,23 +1,28 @@
-# Project 3: Around The U.S.
+# Project 4: Around The U.S.
 
 ### Overview
 
 - Intro
 - Customization
+- JavaScript
 - Figma
 - GitHub
 
 **Intro**
 
-This project is made so all the elements are displayed correctly on popular screen sizes. It is built from scratch and incorporates the use of media queries to create a responsive design layout that adjusts accordingly through breakpoints as you go from a desktop view, to a tablet view, ending in a mobile view.
+This project is made so all the elements are displayed correctly on popular screen sizes. It is built from scratch and incorporates the use of media queries to create a responsive design layout that adjusts accordingly through breakpoints as you go from a desktop view, to a tablet view, ending in a mobile view. The newest addition to the Around the U.S. project is the use of Javascript to display its content and make profile editing functional.
 
 **Customization**
 
 This project moves away from the original Figma content with a personal Avatar photo, name change to reflect the author, and personal photos of locations around the US all while keeping the original layout and design. Since my name is shorter, adjustments had to be made to the profile info section to keep up with the Figma design and responsive layout requirements.
 
+**JavaScript**
+
+This project focussed on using Javascript to create a functioning pop up form that allows the user to edit and save the name and subtext of the profile. It also uses Javascript to display the cards of the page, instead of having them listed in the html file.
+
 **Figma**
 
-- [Link to the project on Figma](https://www.figma.com/file/ii4xxsJ0ghevUOcssTlHZv/Sprint-3%3A-Around-the-US?node-id=0%3A1)
+- [Link to the project on Figma](https://www.figma.com/design/EO5AaNCuzzFL7X5gSY7HwQ/Sprint-4_-Around-The-U.S.-_-desktop-%2B-mobile)
 
 **GitHub**
 
--- scripts/index.js
+++ scripts/index.js
@@ -25,17 +25,57 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+/* ELEMENTS */
 
 const profileEditButton = document.querySelector("#profile-edit-button");
-const profileEditModal = document.querySelector("#profileEditModal");
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditCloseButton = document.querySelector("#modal-close-button");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subtext");
+const profileNameInput = document.querySelector("#profile-title-input");
+const profileDescriptionInput = document.querySelector(
+  "#profile-subtext-input"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListEl = document.querySelector(".elements__list");
+const cardTemplate = document.querySelector("#card-template").content;
+
+/* FUNCTIONS */
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  cardTitleEl.textContent = cardData.name;
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  return cardElement;
+}
+
+/* EVENT LISTENERS */
 
 profileEditButton.addEventListener("click", () => {
+  profileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
   profileEditModal.classList.add("modal_opened");
 });
 
-const profileEditCloseButton = document.querySelector("#modal-close-button");
-
 profileEditCloseButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
+  closePopup();
+});
+
+profileEditForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+});
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListEl.append(cardElement);
 });
